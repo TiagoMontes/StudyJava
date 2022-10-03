05b6e595c464970cb8e7d4f28a9b9692c7e1ffef
TiagoMontes <tiagosantos6303@gmail.com>
2022-10-03 08:34:16 -0500

    Nome, idade, altura e boolean

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main (String[] args) {
+        System.out.println("Nome: João");
+        System.out.print("Idade: ");
+        System.out.println(4);
+        System.out.print("Altura: ");
+        System.out.println(1.09f);
+        System.out.print("Criança: ");
+        System.out.println(true);
     }
 }
